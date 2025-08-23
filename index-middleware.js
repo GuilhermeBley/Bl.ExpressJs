7c8398eef10aa8aee893779d870e4dd50379d088
Guilherme Bley <guilhermecbley@gmail.com>
2025-08-23 11:51:39 -0300

    Adding middleware concept

---
+++ index-middleware.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { dirname } from "path";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+
+const app = express()
+const port = 3001
+const __dir = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true })) // here we're using a middleware to convert form-url-encoded
+
+app.get('/', (req, res) => {
+  res.sendFile(__dir + "/public/index.html")
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}.`)
+})
--- redme.md
+++ redme.md
@@ -4,3 +4,14 @@ Project created for studies purposes
 ## Debug facilities
 To help to debug the project, we can install `npm install -g nodemon`.
 And then, to execute the project you can type `nodemon index.js`.
+
+
+### Express annotations
+Utils Express tools to improve your app quality and development.
+
+### Middlewares
+Runs between the requests, being a valuable resource for global control, logging, so on...
+How to use:
+```js
+app.use(/*put here your middleware handler*/);
+```
