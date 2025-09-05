ad020edafc50b846c99a82c0732ef0b93a10d7e2
Guilherme Bley <guilhermecbley@gmail.com>
2025-09-04 22:20:41 -0300

    feat(login) - login project

---
+++ src/login-app/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
