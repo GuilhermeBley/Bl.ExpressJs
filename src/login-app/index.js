26db8b828d807dc6cfeac0ecf76bb6c18aa448c8
Guilherme Bley <guilhermecbley@gmail.com>
2025-09-10 22:35:21 -0300

    feat - adding views

--- src/login-app/index.js
+++ src/login-app/index.js
@@ -12,10 +12,41 @@ passport.use(new FacebookStrategy({
     clientID: process.env['FACEBOOK_APP_ID'],
     clientSecret: process.env['FACEBOOK_APP_SECRET'],
     callbackURL: 'http://localhost:3000/oauth2/redirect/facebook',
-    state: true
-}), function verify(accessToken, refreshToken, profile, cb) {
-  console.log(`Access token: ${accessToken}; refreshToken: ${refreshToken}; profile: ${profile}`)
-})
+    state: true}), 
+    function verify(accessToken, refreshToken, profile, cb) {
+      console.log(`Access token: ${accessToken}; refreshToken: ${refreshToken}; profile: ${profile}`)
+      return cb(profile, false);
+    })
+
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
+  }
+});
 
 app.get('/login/facebook', passport.authenticate('facebook'));
 
